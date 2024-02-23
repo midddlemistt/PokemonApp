768c89378a0bea8db3fdfbd9dc9f47b3f668277e
me <pashatimofeeff@gmail.com>
2024-02-24 00:43:43 +0300

    Created PokemonManager for pokemons data managment

--- PokemonApp/Managers/PokemonManager.swift
+++ PokemonApp/Managers/PokemonManager.swift
@@ -8,5 +8,18 @@
 import Foundation
 
 class PokemonManager {
+    func getPokemon() -> [Pokemon] {
+        let data: PokemonPage = Bundle.main.decode(file: "pokemon.json")
+        let pokemon: [Pokemon] = data.results
+        
+        return pokemon
+    }
     
+    func getDetailedPokemon(id: Int, _ completition:@escaping (DetailPokemon) -> ()) {
+        Bundle.main.fetchData(url: "https://pokeapi.co/api/v2/pokemon/\(id)/", model: DetailPokemon.self) {data in
+            completition(data)
+        } failure: { error in
+            print(error)
+        }
+    }
 }
