7f07875a37154dbc45a613bac705a7c96bbf7b79
me <pashatimofeeff@gmail.com>
2024-02-23 22:23:05 +0300

    created PokemonMode;l

--- PokemonApp/Models/PokemonModel.swift
+++ PokemonApp/Models/PokemonModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct PokemonPage: Codable {
+    let count: Int
+    let next: String
+    let results: [Pokemon]
+}
+
+struct Pokemon: Codable, Identifiable, Equatable {
+    let id = UUID()
+    let name: String
+    let url: String
+    
+    static var samplePokemon = Pokemon(name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/")
+}
+
+struct DetailPokemon: Codable {
+    let type: String
+    let weight: Int
+    let height: Int
+}
