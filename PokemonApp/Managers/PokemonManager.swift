fb747da8db62d22f2460ef66a9159aff9e1c8032
me <pashatimofeeff@gmail.com>
2024-02-23 22:22:15 +0300

    created pokemonModel

---
+++ PokemonApp/Managers/PokemonManager.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonManager.swift
+//  PokemonApp
+//
+//  Created by 123 on 23.02.24.
+//
+
+import Foundation
---
+++ PokemonApp/Models/PokemonModel.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonModel.swift
+//  PokemonApp
+//
+//  Created by 123 on 23.02.24.
+//
+
+import Foundation
